06298df344f2eb65a9d7845293f50833765b5e0a
fayuan.fzw <motivated24@hotmail.com>
2018-01-21 23:28:07 +0800

    Enum枚举

---
+++ src/no001_Enum/Day.java
@@ -0,0 +1,21 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package no001_Enum;
+
+/**
+ *
+ * @author mr.24
+ * @version $Id: Day.java, v 0.1 2018年01月21日 下午10:56 mr.24 Exp $
+ */
+public enum Day {
+    /** 周一 */
+    MONDAY,
+
+    /** 周二 */
+    TUESDAY,
+
+    /** 周三 */
+    WEDNESDAY
+}
