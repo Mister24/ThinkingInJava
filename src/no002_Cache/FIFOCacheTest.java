986e098295b1a252021b02bc77f2679b082b15bd
fayuan.fzw <motivated24@hotmail.com>
2018-01-31 23:30:24 +0800

    FIFOCache测试

---
+++ src/no002_Cache/FIFOCacheTest.java
@@ -0,0 +1,20 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package no002_Cache;
+
+/**
+ *
+ * @author mr.24
+ * @version $Id: FIFOCacheTest.java, v 0.1 2018年01月26日 上午12:33 mr.24 Exp $
+ */
+public class FIFOCacheTest {
+    public static void main(String[] args) {
+        FIFOCache<Integer, Integer> map = new FIFOCache<Integer, Integer>(5);
+        for (int i = 0; i ++ < 10;) {
+            map.put(i, i);
+        }
+        //System.out.println("初始存储情况：" + map.toString);
+    }
+}
