0f6a0e1932a0e53ccbfc6c41192f5a4d9a9bb16e
Mister24 <motivated24@hotmail.com>
2018-03-21 00:15:50 +0800

    增加注释

--- src/interview/com/fayuan/singleton/Singleton2.java
+++ src/interview/com/fayuan/singleton/Singleton2.java
@@ -8,7 +8,7 @@ package interview.com.fayuan.singleton;
  * 单线程懒汉式（可以延迟加载，但线程不安全）
  * 私有构造器 + 公有静态工厂方法
  * 在工厂方法中对单例进行判空，如果为null，则新建
- * 如果多个线程同时调用工厂方法，很可能导致重复创建对象，所以线程不安全
+ * 问题：如果多个线程同时调用工厂方法，很可能导致重复创建对象，所以线程不安全
  *
  * @author fayuan.fzw
  * @version $Id: Singleton2.java, v 0.1 2018年03月20日 下午11:18 fayuan.fzw Exp $
