0954b55a5675c8aff91430c02658600c4d19836f
Mister24 <motivated24@hotmail.com>
2018-05-31 20:47:48 +0800

    二叉树

---
+++ src/main/java/com/fayuan/interview/binaryTree/treeNode/TreeNode.java
@@ -0,0 +1,81 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.fayuan.interview.binaryTree.treeNode;
+
+/**
+ * 二叉树节点类
+ *
+ * @author fayuan.fzw
+ * @version $Id: TreeNode.java, v 0.1 2018年03月27日 下午8:41 fayuan.fzw Exp $
+ */
+public class TreeNode {
+
+    //节点值
+    public int val;
+
+    //左子树
+    public TreeNode left = null;
+
+    //右子树
+    public TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    /**
+     * Getter method for property <tt>val</tt>.
+     *
+     * @return property value of val
+     */
+    public int getVal() {
+        return val;
+    }
+
+    /**
+     * Setter method for property <tt>val</tt>.
+     *
+     * @param val  value to be assigned to property val
+     */
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    /**
+     * Getter method for property <tt>left</tt>.
+     *
+     * @return property value of left
+     */
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    /**
+     * Setter method for property <tt>left</tt>.
+     *
+     * @param left  value to be assigned to property left
+     */
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    /**
+     * Getter method for property <tt>right</tt>.
+     *
+     * @return property value of right
+     */
+    public TreeNode getRight() {
+        return right;
+    }
+
+    /**
+     * Setter method for property <tt>right</tt>.
+     *
+     * @param right  value to be assigned to property right
+     */
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+}
