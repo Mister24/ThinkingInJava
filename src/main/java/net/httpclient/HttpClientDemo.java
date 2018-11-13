6771f9a57899fd025e5448c2a2621d8711f6bf4e
Mister24 <motivated24@hotmail.com>
2018-11-13 11:16:27 +0800

    httpclient使用

---
+++ src/main/java/net/httpclient/HttpClientDemo.java
@@ -0,0 +1,15 @@
+/**
+ * @Company Mister24.com Inc.
+ * @Copyright Copyright (c) 2016-2018 All Rights Reserved.
+ */
+package net.httpclient;
+
+/**
+ * @author mr.24
+ * @version Id: HttpClientDemo, v 1.0 2018/10/28 下午4:21 Exp $$
+ */
+public class HttpClientDemo {
+    public static void main(String[] args) {
+        //HttpClient httpClient = new HttpClient();
+    }
+}
