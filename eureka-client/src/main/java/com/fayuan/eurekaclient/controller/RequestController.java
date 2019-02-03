b75b63bd64a8b25d4f1ad541812b08c708154172
Mister24 <motivated24@hotmail.com>
2019-02-03 21:30:57 +0800

    eureka client

---
+++ eureka-client/src/main/java/com/fayuan/eurekaclient/controller/RequestController.java
@@ -0,0 +1,25 @@
+/**
+ * @Company Mister24.com Inc.
+ * @Copyright Copyright (c) 2016-2019 All Rights Reserved.
+ */
+package com.fayuan.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mr.24
+ * @version Id: RequestController, v 1.0 2019/2/3 下午8:23 Exp $$
+ */
+@RestController
+public class RequestController {
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi" + name + ", I am port:" + port;
+    }
+}
