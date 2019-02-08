13cf5edcae3e364745cbd02d6d99df35c2924f4f
Mister24 <motivated24@hotmail.com>
2019-02-08 18:52:46 +0800

    对ribbon客户端的RestTemplate添加LoadBalanced来开启客户端负载均衡功能

--- ribbon-client/src/main/java/com/fayuan/ribbonclient/RibbonClientApplication.java
+++ ribbon-client/src/main/java/com/fayuan/ribbonclient/RibbonClientApplication.java
@@ -2,13 +2,24 @@ package com.fayuan.ribbonclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RibbonClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RibbonClientApplication.class, args);
     }
 
+    @Bean
+    // 开启客户端负载均衡功能
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
 
