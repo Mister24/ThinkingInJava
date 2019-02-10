7efd6ec0a41899c01016cba3e4b255a1b9579b70
Mister24 <motivated24@hotmail.com>
2019-02-10 22:53:36 +0800

    feign依赖修改

--- feign-server/src/main/java/com/fayuan/feignserver/FeignServerApplication.java
+++ feign-server/src/main/java/com/fayuan/feignserver/FeignServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FeignServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FeignServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FeignServerApplication.class, args);
+    }
 
 }
 
