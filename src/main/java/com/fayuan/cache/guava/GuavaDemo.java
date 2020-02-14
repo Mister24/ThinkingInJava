e94a4e4d206d0fbd034c5e90df5309d28826b0df
Mister24 <motivated24@hotmail.com>
2020-02-14 21:52:11 +0800

    optional

---
+++ src/main/java/com/fayuan/cache/guava/GuavaDemo.java
@@ -0,0 +1,12 @@
+/**
+ * @Company Mister24.com Inc.
+ * @Copyright Copyright (c) 2016-2019 All Rights Reserved.
+ */
+package com.fayuan.cache.guava;
+
+/**
+ * @author mr.24
+ * @version Id: GuavaDemo, v 1.0 2019-12-14 15:10 Exp $$
+ */
+public class GuavaDemo {
+}
---
+++ src/main/java/lambdas/chapter10_optional/domain/Insurance.java
@@ -0,0 +1,24 @@
+/**
+ * @Company Mister24.com Inc.
+ * @Copyright Copyright (c) 2016-2019 All Rights Reserved.
+ */
+package lambdas.chapter10_optional.domain;
+
+/**
+ * insurance
+ *
+ * @author mr.24
+ * @version Id: Insurance, v 1.0 2019-10-29 00:25 Exp $$
+ */
+public class Insurance {
+    private String name;
+
+    /**
+     * Getter method for property <tt>name</tt>.
+     *
+     * @return property value of name
+     */
+    public String getName() {
+        return name;
+    }
+}
